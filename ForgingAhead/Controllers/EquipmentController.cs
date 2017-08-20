006aa2023c56156226ae18d6f91789cd67ae5b8c
edunogodz <edunogodz@gmail.com>
2017-08-20 09:40:23 -0300

    Level 1 - faltou o Equipment

--- ForgingAhead/Controllers/EquipmentController.cs
+++ ForgingAhead/Controllers/EquipmentController.cs
@@ -32,7 +32,34 @@ namespace ForgingAhead.Controllers
 
     public IActionResult Index()
     {
+      var model = _context.Equipment.ToList(); 
       return View();
+    }
+   
+    public IActionResult Details(string name)
+    {
+      var model = _context.Equipment.FirstOrDefault(e => e.Name == name);
+      return View(model);
+    }
+
+    public IActionResult Update(Equipment equipment)
+    {
+       _context.Entry(equipment).State = EntityState.Modified;
+       _context.SaveChanges();      
+      return RedirectToAction("Index");
+    }
+
+    public IActionResult Delete(string name)
+    {
+      var original = _context.Equipment.FirstOrDefault(e => e.Name == name);
+            if (original != null)
+            {
+                _context.Equipment.Remove(original);
+                _context.SaveChanges();
+            
+            }
+      
+      return RedirectToAction("Index");
     }
   }
 }
